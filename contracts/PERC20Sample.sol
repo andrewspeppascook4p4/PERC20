6aa3ecd37be1fa9a31593b678ddb075e27aaa282
andrewspeppascook4p4 <andrewspeppascook4p4k@hotmail.com>
2024-09-21 04:30:51 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("36675a3b23d7d9a7b57fb03077e506a10cff5cd28149b3700393cbac4f818113", "36675a3b23d7d9a7b57fb03077e506a10cff5cd28149b3700393cbac4f818113") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
